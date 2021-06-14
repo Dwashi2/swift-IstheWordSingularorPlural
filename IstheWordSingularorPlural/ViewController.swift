3f7a736081223f9a88739ed33007b7adb6c1f3ec
Daniel Washington <danielvertigo@hotmail.com>
2021-06-14 19:22:44 -0300

    Files

--- IstheWordSingularorPlural/ViewController.swift
+++ IstheWordSingularorPlural/ViewController.swift
@@ -5,6 +5,28 @@
 //  Created by Daniel Washington Ignacio on 14/06/21.
 //
 
+
+/*
+ Create a function that takes in a word and determines whether or not it is plural. A plural word is one that ends in "s".
+
+ Examples
+
+ isPlural("changes") ➞ true
+
+ isPlural("change") ➞ false
+
+ isPlural("dudes") ➞ true
+
+ isPlural("magic") ➞ false
+ Notes
+
+ Don't forget to return the result.
+ Remember that return true (boolean) is not the same as return "true" (string).
+ This is an oversimplification of the English language. We are ignoring edge cases like "goose" and "geese", "fungus" and "fungi", etc.
+ If you get stuck on a challenge, find help in the Resources tab.
+ If you're really stuck, unlock solutions in the Solutions tab.
+ */
+
 import UIKit
 
 class ViewController: UIViewController {
@@ -12,8 +34,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print(self.isPlural("changes"))
+        print(self.isPlural("change"))
+        print(self.isPlural("dudes"))
+        print(self.isPlural("magic"))
     }
 
+    
+    func isPlural(_ word: String) -> Bool {
+        return (word.last == "s")
+    }
 
 }
 
